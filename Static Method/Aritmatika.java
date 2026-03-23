ec7387f26be8a958443b364ffddddf602ce2e0d0
Nurrohim Matorikul <nurrohimmatorikul@gmail.com>
2026-03-23 09:34:46 +0700

    Add files via upload

---
+++ Static Method/Aritmatika.java
@@ -0,0 +1,19 @@
+public class Aritmatika {
+    public void hitungPenjumlahan(int a,int b){
+        int nilai = a+b;
+        System.out.println("nilai penjumlahan adalah : "+nilai);
+        hitungPerkalian(a,b);
+    }
+    public static void hitungPerkalian(int a, int b){
+        int nilai = a*b;
+        System.out.println("nilai perkalian adalah : "+nilai);
+    }
+    public static void hitungPengurangan(int a, int b){
+        int nilai = a-b;
+        System.out.println("nilai pengurangan adalah : "+nilai);
+    }
+    public double hitungPembagian(String nil, String nil2){
+        double nilai = Double.parseDouble(nil)/Double.parseDouble(nil2);
+        return nilai;
+    }
+}
