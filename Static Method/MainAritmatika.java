5c0d068aff6b78f2a075919e2b00a59ee62fdf17
Nurrohim Matorikul <nurrohimmatorikul@gmail.com>
2026-03-23 09:23:06 +0700

    Add files via upload

---
+++ Static Method/MainAritmatika.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class MainAritmatika {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("masukkan nilai 1 : ");
+        int nil1 = in.nextInt();
+        System.out.print("masukkan nilai 2 : ");
+        int nil2 = in.nextInt();
+        //memanggil method static
+        Aritmatika.hitungPengurangan(nil1, nil2);
+
+        System.out.print("masukkan nilai 1 : ");
+        nil1 = in.nextInt();
+        System.out.print("masukkan nilai 2 : ");
+        nil2 = in.nextInt();
+        //memanggil method static
+        Aritmatika.hitungPerkalian(nil1, nil2);
+
+        System.out.print("masukkan nilai 1 : ");
+        int value1 = in.nextInt();
+        System.out.print("masukkan nilai 2 : ");
+        int value2 = in.nextInt();
+        //memanggil method NONstatic harus melalui objek
+        Aritmatika a = new Aritmatika();
+        a.hitungPenjumlahan(value1, value2);
+
+        // System.out.print("masukkan nilai 1 : ");
+        // String Nil = in.next();
+        // System.out.print("masukkan nilai 2 : ");
+        // String Nil2 = in.next();
+        // double bagi = a.hitungPembagian(Nil, Nil2);
+        // System.out.println("nilai pembagian adalah : " +bagi);
+    }
+}
